7e85b1ff2ee6a499e9d45654ecf2cb50d458f6af
Alex Salcedo <awsalcedo@sri.gob.ec>
2022-09-23 11:30:53 -0500

    Verify active session

--- app/src/main/java/com/asalcedo/nilopartner/MainActivity.kt
+++ app/src/main/java/com/asalcedo/nilopartner/MainActivity.kt
@@ -9,25 +9,53 @@ import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var authStateListener: FirebaseAuth.AuthStateListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
+        configAuth()
 
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            val response = IdpResponse.fromResultIntent(it.data)
-            if (it.resultCode == RESULT_OK) {
-                //Verify user authenticated
-                val user = FirebaseAuth.getInstance().currentUser
-                if (user != null) {
-                    Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
-                }
+    }
+
+    private fun configAuth() {
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        authStateListener = FirebaseAuth.AuthStateListener { auth ->
+            if (auth.currentUser != null) {
+                supportActionBar?.title = auth.currentUser?.displayName
+            } else {
+                val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
+
+                registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                    val response = IdpResponse.fromResultIntent(it.data)
+                    if (it.resultCode == RESULT_OK) {
+                        //Verify user authenticated
+                        val user = FirebaseAuth.getInstance().currentUser
+                        if (user != null) {
+                            Toast.makeText(this, "Welcome", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                }.launch(
+                    AuthUI.getInstance().createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build()
+                )
             }
+        }
+    }
 
-        }.launch(
-            AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers)
-                .build()
-        )
+    override fun onResume() {
+        super.onResume()
+        firebaseAuth.addAuthStateListener(authStateListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        firebaseAuth.removeAuthStateListener(authStateListener)
     }
 }
