d131f9ac30182e296141ef536cd9a4a58c20a367
Alex Salcedo <awsalcedo@sri.gob.ec>
2022-10-03 14:07:46 -0500

    Authentication with Google and fingerprint SHA1

--- app/src/main/java/com/asalcedo/nilopartner/MainActivity.kt
+++ app/src/main/java/com/asalcedo/nilopartner/MainActivity.kt
@@ -40,7 +40,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         configAuth()
 
     }
